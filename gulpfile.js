d15b7706a986b829194402dd601467a6bf6fadc9
jasper <jasper.alani@colart.com>
2020-07-11 21:39:20 +0100

    gulpfile.js for scss compiling

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const changed = require('gulp-changed');
+const minify = require('gulp-minify');
+
+const pre_path = './react/';
+
+const sass_SOURCE = pre_path + 'src/scss/**/*';
+const sass_THEME = pre_path + 'src/scss/theme.scss';
+const sass_DISTRIBUTION = pre_path + 'src/css';
+
+gulp.task('copy-sass', () => {
+  return gulp.src(sass_THEME)
+    .pipe(sass())
+    .pipe(gulp.dest(sass_DISTRIBUTION));
+});
+
+gulp.task('watch-sass', () => {
+  gulp.watch(sass_SOURCE, gulp.series('copy-sass'));
+});
