35a2e4225ff151116603efccde49c91d717ae0b5
jasper <jasper.alani@colart.com>
2020-07-18 21:11:47 +0100

    go.sql

--- mariadb/go.sql
+++ mariadb/go.sql
@@ -5,11 +5,11 @@ create table items
 (
     id        int auto_increment
         primary key,
-    name      varchar(255) null,
-    url       varchar(255) null,
-    image_url varchar(255) null,
-    person    varchar(255) null,
-    quantity  int          null,
+    name      varchar(255) null default '',
+    url       varchar(255) null default '',
+    image_url varchar(255) null default '',
+    person    varchar(255) null default '',
+    quantity  int          null default 1,
     created   timestamp         default current_timestamp,
     deleted   tinyint(1)   null default false,
     completed tinyint(1)   null default false
